db41d4d935b932abfd1a09645d0b2b8d5cfcf501
php1301 <19520854@gm.uit.edu.vn>
2020-06-22 22:23:22 +0700

    Module config for app config

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,40 @@ import { AuthModule } from './auth/auth.module';
 import { APP_PIPE } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+
+// document cho mail https://nest-modules.github.io/mailer/docs/mailer.html
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
+
 // Để validation work xuyên suốt app thì phải provide validation pipe
 @Module({
   imports: [
+    // Bất đồng bộ, xài forRoot sẽ dính lỗi Plain => do auth ko lấy dc .env
+    MailerModule.forRootAsync({
+      useFactory: () => ({
+        transport: {
+          host: 'smtp.gmail.com',
+          port: 587,
+          secure: false, // upgrade later with STARTTLS
+          auth: {
+            user: process.env.LOCAL_EMAIL,
+            pass: process.env.LOCAL_PASSWORD,
+          },
+        },
+        defaults: {
+          from: '"nest-modules" <modules@nestjs.com>',
+        },
+        template: {
+          // dir: process.cwd() + '/templates/',
+          adapter: new PugAdapter(), // or new PugAdapter()
+          options: {
+            strict: true,
+          },
+        },
+      }),
+    }),
     ConfigModule.forRoot({
-      envFilePath:'./.env'
+      envFilePath: './.env',
     }),
     GraphQLModule.forRootAsync({
       useClass: GraphqlOptions,
