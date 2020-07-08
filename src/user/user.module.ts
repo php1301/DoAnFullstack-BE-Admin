c910bdc7d40c0ac7776eb0b4a92a3958200e1144
php1301 <19520854@gm.uit.edu.vn>
2020-07-08 21:23:03 +0700

    user module redis added

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,9 +2,27 @@ import { Module } from '@nestjs/common';
 import { UserResolver } from './user.resolver';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { MailService } from 'src/services/sendEmail';
-
+import { RedisPubSub } from 'graphql-redis-subscriptions';
+import * as Redis from 'ioredis';
 @Module({
-  providers: [UserResolver, MailService],
+  providers: [
+    UserResolver,
+    MailService,
+    {
+      provide: 'PUB_SUB',
+      useFactory: () => {
+        const options = {
+          host: 'localhost',
+          port: 6379,
+        };
+        return new RedisPubSub({
+          publisher: new Redis(options),
+          subscriber: new Redis(options),
+        });
+      },
+      // useValue: new PubSub(),
+    },
+  ],
   imports: [PrismaModule],
 })
 export class UserModule {}
