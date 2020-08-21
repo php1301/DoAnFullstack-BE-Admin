043f761069793225a89e17621ea4bdb466174fc4
php1301 <19520854@gm.uit.edu.vn>
2020-08-21 23:54:51 +0700

    generated schema data

--- src/graphql.schema.generated.ts
+++ src/graphql.schema.generated.ts
@@ -8,20 +8,31 @@
 /* eslint-disable */
 export class AddHotelInput {
     hotelName?: string;
-    pricePerNight?: string;
+    pricePerNight?: number;
     hotelDetails?: string;
     guest?: number;
-    beds?: number;
+    rooms?: number;
     price?: number;
     hotelPhotos?: ImageInput[];
     location?: LocationInput[];
     locationDescription?: string;
     contactNumber?: string;
+    propertyType?: string;
+    isNegotiable?: boolean;
     wifiAvailability?: boolean;
     airCondition?: boolean;
     parking?: boolean;
     poolAvailability?: boolean;
     extraBed?: boolean;
+}
+
+export class AmenitiesSearchInput {
+    wifiAvailability?: boolean;
+    poolAvailability?: boolean;
+    parkingAvailability?: boolean;
+    airCondition?: boolean;
+    rooms?: number;
+    guest?: number;
 }
 
 export class CategoriesInput {
@@ -31,10 +42,32 @@ export class CategoriesInput {
 }
 
 export class ContactInput {
-    targetEmail: string;
+    subject: string;
     message?: string;
     email?: string;
     cellNumber?: string;
+}
+
+export class CouponCheckedPayload {
+    couponId?: string;
+    couponName?: string;
+    couponValue?: number;
+    couponType?: number;
+}
+
+export class CouponInput {
+    couponName: string;
+    couponDescription?: string;
+    couponType?: number;
+    couponValue?: number;
+    couponQuantity?: number;
+    couponStartDate?: string;
+    couponEndDate?: string;
+    couponRange?: number;
+}
+
+export class DeletePhotosInput {
+    id: string;
 }
 
 export class ImageInput {
@@ -42,8 +75,8 @@ export class ImageInput {
 }
 
 export class LocationInput {
-    lat?: string;
-    lng?: string;
+    lat?: number;
+    lng?: number;
     formattedAddress?: string;
     zipcode?: string;
     city?: string;
@@ -58,12 +91,69 @@ export class LoginInput {
     password: string;
 }
 
+export class MockInput {
+    title?: string[];
+}
+
+export class ReviewFieldInput {
+    rating?: number;
+    ratingFieldName?: string;
+}
+
+export class ReviewInput {
+    reviewOverall?: number;
+    reviewTitle?: string;
+    reviewTips?: string;
+    reviewText?: string;
+    sortOfTrip?: string;
+    reviewFieldInput?: ReviewFieldInput[];
+    reviewOptionals?: ReviewOptionalsInput[];
+    reviewPics?: ImageInput[];
+}
+
+export class ReviewOptionalsInput {
+    option?: string;
+    optionField?: string;
+}
+
+export class SearchInput {
+    minPrice?: number;
+    maxPrice?: number;
+}
+
 export class SignUpInput {
     username?: string;
     first_name?: string;
     last_name?: string;
     email: string;
     password: string;
+    profile_pic_main?: string;
+    cover_pic_main?: string;
+}
+
+export class SocialInput {
+    facebook?: string;
+    instagram?: string;
+    twitter?: string;
+}
+
+export class TransactionInput {
+    transactionHotelName?: string;
+    transactionHotelManagerId?: string;
+    transactionHotelType?: string;
+    transactionPrice?: number;
+    transactionAuthorName?: string;
+    transactionAuthorEmail?: string;
+    transactionAuthorContactNumber?: string;
+    transactionAuthorSpecial?: string;
+    transactionAuthorNote?: string;
+    transactionLocationFormattedAddress?: string;
+    transactionLocationLat?: number;
+    transactionLocationLng?: number;
+    transactionRange?: number;
+    transactionStartDate?: string;
+    transactionEndDate?: string;
+    transactionStripeId?: string;
 }
 
 export class UpdatePassword {
@@ -73,8 +163,9 @@ export class UpdatePassword {
 }
 
 export class UpdatePhotosInput {
-    cover_pic?: ImageInput[];
-    profile_pic?: ImageInput[];
+    url?: string;
+    uid?: string;
+    signedRequest?: string;
 }
 
 export class UpdateProfileInput {
@@ -100,8 +191,12 @@ export class Amenities {
 }
 
 export class AuthPayload {
-    id: string;
-    email: string;
+    id?: string;
+    email?: string;
+    first_name?: string;
+    last_name?: string;
+    profile_pic_main?: string;
+    cover_pic_main?: string;
 }
 
 export class Categories {
@@ -111,21 +206,44 @@ export class Categories {
     image?: Image;
 }
 
+export class Coupon {
+    couponId: string;
+    couponName: string;
+    couponDescription?: string;
+    couponAuthor?: User;
+    couponAuthorId?: string;
+    couponType?: number;
+    couponValue?: number;
+    couponQuantity?: number;
+    couponStartDate?: string;
+    couponEndDate?: string;
+    couponRange?: number;
+    couponTarget?: Hotel[];
+    createdAt: string;
+    updatedAt: string;
+}
+
 export class Gallery {
     id: string;
+    uid?: string;
     url?: string;
+    signedRequest?: string;
 }
 
 export class Hotel {
     id: string;
     peopleLiked?: User[];
+    peopleReviewed?: User[];
+    couponsAvailable?: Coupon[];
     connectId?: User;
     agentId?: string;
+    agentEmail?: string;
+    agentName?: string;
     title?: string;
     slug?: string;
     status?: string;
     content?: string;
-    price?: string;
+    price?: number;
     isNegotiable?: boolean;
     propertyType?: string;
     condition?: string;
@@ -134,10 +252,11 @@ export class Hotel {
     contactNumber?: string;
     termsAndCondition?: string;
     amenities?: Amenities[];
-    image?: Image[];
+    image?: Image;
     location?: Location[];
     gallery?: Gallery[];
     categories?: Categories[];
+    reviews?: Reviews[];
     createdAt: string;
     updatedAt: string;
 }
@@ -149,12 +268,13 @@ export class HotelPhotos {
 export class Image {
     id: string;
     url?: string;
+    thumb_url?: string;
 }
 
 export class Location {
     id: string;
-    lat?: string;
-    lng?: string;
+    lat?: number;
+    lng?: number;
     formattedAddress?: string;
     zipcode?: string;
     city?: string;
@@ -164,6 +284,18 @@ export class Location {
     country_short?: string;
 }
 
+export class Message {
+    reviewAuthorName?: string;
+    reviewAuthorId?: string;
+    reviewedHotelId?: Hotel;
+    reviewedHotelName?: string;
+    hotelManagerId?: string;
+    reviewTitle?: string;
+    reviewText?: string;
+    peopleReviewedQuanity?: number;
+    peopleReviewedArr?: User[];
+}
+
 export abstract class IMutation {
     abstract signup(signUpInput?: SignUpInput): AuthPayload | Promise<AuthPayload>;
 
@@ -171,11 +303,25 @@ export abstract class IMutation {
 
     abstract createHotel(addHotelInput?: AddHotelInput, location?: LocationInput[], image?: ImageInput[], categories?: CategoriesInput[]): Hotel | Promise<Hotel>;
 
+    abstract sortHotel(type?: string): Hotel[] | Promise<Hotel[]>;
+
+    abstract likeHotel(id: string): Hotel | Promise<Hotel>;
+
+    abstract dislikeHotel(id: string): Hotel | Promise<Hotel>;
+
+    abstract filterHotels(search?: SearchInput, amenities?: AmenitiesSearchInput): Hotel[] | Promise<Hotel[]>;
+
     abstract createLocation(location?: LocationInput): Location | Promise<Location>;
 
-    abstract updateProfile(profile?: UpdateProfileInput, location?: LocationInput): User | Promise<User>;
+    abstract updateProfile(profile?: UpdateProfileInput, location?: LocationInput, social?: SocialInput): User | Promise<User>;
 
-    abstract updatePhotos(photos?: UpdatePhotosInput): User | Promise<User>;
+    abstract updatePhotos(photos?: UpdatePhotosInput[]): User | Promise<User>;
+
+    abstract deletePhotos(photos?: DeletePhotosInput[]): User | Promise<User>;
+
+    abstract setProfilePic(url?: string): User | Promise<User>;
+
+    abstract setCoverPic(url?: string): User | Promise<User>;
 
     abstract updatePassword(password?: UpdatePassword): User | Promise<User>;
 
@@ -183,13 +329,57 @@ export abstract class IMutation {
 
     abstract sendContact(contact?: ContactInput): User | Promise<User>;
 
-    abstract likeHotel(id?: string): Hotel | Promise<Hotel>;
+    abstract makeReviews(reviews?: ReviewInput, hotelId?: string): Hotel | Promise<Hotel>;
 
-    abstract dislikeHotel(id: string): Hotel | Promise<Hotel>;
+    abstract likeOrDislikeReview(id: string, type?: number): Reviews | Promise<Reviews>;
+
+    abstract checkNotification(id: string): User | Promise<User>;
+
+    abstract readNotification(query?: string): User | Promise<User>;
+
+    abstract deleteAllNotifications(id: string): User | Promise<User>;
+
+    abstract createTransaction(transaction?: TransactionInput, hotelId?: string, userId?: string, coupon?: CouponCheckedPayload): Transaction | Promise<Transaction>;
+
+    abstract createCoupon(coupon?: CouponInput, hotelsId?: string[], type?: number): Coupon[] | Promise<Coupon[]>;
+
+    abstract checkCoupon(hotelId?: string, couponName?: string): Coupon | Promise<Coupon>;
+
+    abstract processTransactions(id?: string[], type?: number): Transaction | Promise<Transaction>;
+
+    abstract deleteCoupons(id?: string[]): Coupon[] | Promise<Coupon[]>;
+
+    abstract updateStripeId(stripeId?: string, type?: string): User | Promise<User>;
+
+    abstract ping(): Ping | Promise<Ping>;
+}
+
+export class Notification {
+    id: string;
+    reviewAuthorName?: string;
+    reviewedHotelName?: string;
+    reviewTitle?: string;
+    reviewText?: string;
+    userNotificationId?: string;
+    peopleReviewedQuantity?: number;
+    query?: string;
+    reviewAuthorProfilePic?: string;
+    read?: boolean;
+    old?: boolean;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export class Ping {
+    id?: string;
+}
+
+export class Pong {
+    pingId?: string;
 }
 
 export abstract class IQuery {
-    abstract amenities(): Amenities[] | Promise<Amenities[]>;
+    abstract allAmenities(): Amenities[] | Promise<Amenities[]>;
 
     abstract imageId(id: string): Hotel | Promise<Hotel>;
 
@@ -203,18 +393,135 @@ export abstract class IQuery {
 
     abstract categoryId(id: string): Categories | Promise<Categories>;
 
-    abstract categories(): Categories[] | Promise<Categories[]>;
+    abstract allCategories(): Categories[] | Promise<Categories[]>;
 
     abstract userPosts(id: string): User | Promise<User>;
 
+    abstract favouritePosts(id: string): User | Promise<User>;
+
+    abstract favouritePostsHeart(id: string): User | Promise<User>;
+
     abstract getUserInfo(id: string): User | Promise<User>;
+
+    abstract getUserGallery(id: string): User | Promise<User>;
+
+    abstract getUserReviews(id: string): User | Promise<User>;
+
+    abstract getUserNotification(id: string): Notification[] | Promise<Notification[]>;
+
+    abstract getUserUnreadNotification(id: string): User | Promise<User>;
+
+    abstract getHotelInfo(id: string): Hotel | Promise<Hotel>;
+
+    abstract getHotelReviews(id: string): Reviews[] | Promise<Reviews[]>;
+
+    abstract getHotelCoupons(id: string): Coupon[] | Promise<Coupon[]>;
+
+    abstract getHotelManagerCoupons(): Coupon[] | Promise<Coupon[]>;
+
+    abstract getReviewsLikeDislike(id: string): Reviews | Promise<Reviews>;
+
+    abstract getAllHotels(location?: LocationInput, type?: string, search?: SearchInput, amenities?: AmenitiesSearchInput, property?: string[]): Hotel[] | Promise<Hotel[]>;
+
+    abstract getFilteredHotels(location?: string, search?: SearchInput, amenities?: AmenitiesSearchInput, property?: string[]): Hotel[] | Promise<Hotel[]>;
+
+    abstract getTransactionsHaving(orderBy?: string): Transaction[] | Promise<Transaction[]>;
+
+    abstract getTransactionDetails(transactionSecretKey?: string): Transaction[] | Promise<Transaction[]>;
+
+    abstract noop(): boolean | Promise<boolean>;
+}
+
+export class ReviewFields {
+    id: string;
+    rating?: number;
+    ratingFieldName?: string;
+}
+
+export class ReviewOptionals {
+    id: string;
+    option?: string;
+    optionField?: string;
+}
+
+export class Reviews {
+    reviewID: string;
+    reviewTitle?: string;
+    reviewText?: string;
+    peopleLiked?: User[];
+    peopleDisliked?: User[];
+    reviewTips?: string;
+    sortOfTrip?: string;
+    reviewAuthorId?: User;
+    reviewAuthorFirstName?: string;
+    reviewAuthorLastName?: string;
+    reviewAuthorEmail?: string;
+    reviewOverall?: number;
+    reviewAuthorPic?: string;
+    reviewedHotel?: Hotel;
+    reviewedHotelId?: string;
+    reviewPics?: Image[];
+    reviewDate?: string;
+    reviewOptional?: ReviewOptionals[];
+    reviewFields?: ReviewFields[];
+    createdAt: string;
+    updatedAt: string;
 }
 
 export class Social {
     facebook?: string;
     twitter?: string;
-    linkedIN?: string;
+    linkedIn?: string;
     instagram?: string;
+}
+
+export abstract class ISubscription {
+    abstract pong(): Pong | Promise<Pong>;
+
+    abstract unreadNotification(channelId?: string): Unread | Promise<Unread>;
+
+    abstract notificationBell(channelId?: string): Notification | Promise<Notification>;
+
+    abstract realtimeReviews(hotelId?: string): Reviews | Promise<Reviews>;
+
+    abstract realtimeLikeDislike(reviewID?: string): Reviews | Promise<Reviews>;
+
+    abstract realtimeNotiTransaction(userId?: string): Notification | Promise<Notification>;
+}
+
+export class Transaction {
+    TXID: string;
+    transactionSecretKey?: string;
+    transactionHotelName?: string;
+    transactionHotelManager?: User;
+    transactionHotelManagerId?: string;
+    transactionHotelId?: string;
+    transactionHotelType?: string;
+    transactionPrice?: number;
+    transactionAuthor?: User;
+    transactionAuthorId?: string;
+    transactionAuthorName?: string;
+    transactionAuthorEmail?: string;
+    transactionAuthorContactNumber?: string;
+    transactionAuthorSpecial?: string;
+    transactionAuthorNote?: string;
+    transactionLocationLat?: number;
+    transactionLocationLng?: number;
+    transactionLocationFormattedAddress?: string;
+    transactionRange?: number;
+    transactionStatus?: string;
+    transactionCoupon?: string;
+    transactionCouponType?: number;
+    transactionCouponValue?: number;
+    transactionStartDate?: string;
+    transactionEndDate?: string;
+    transactionStripeId?: string;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export class Unread {
+    unreadNotification?: number;
 }
 
 export class User {
@@ -222,9 +529,13 @@ export class User {
     first_name: string;
     last_name: string;
     username: string;
+    stripeId?: string;
     password: string;
     email: string;
+    role?: string;
     cellNumber?: string;
+    profile_pic_main?: string;
+    cover_pic_main?: string;
     profile_pic?: Image[];
     cover_pic?: Image[];
     date_of_birth?: string;
@@ -233,8 +544,17 @@ export class User {
     agent_location?: Location;
     gallery?: Gallery[];
     social_profile?: Social;
+    reviews_maked?: ReviewFields[];
     listed_posts?: Hotel[];
+    notification?: Notification[];
+    unreadNotification?: number;
     favourite_post?: Hotel[];
+    reviewed_post?: Hotel[];
+    review_liked?: Reviews[];
+    review_disliked?: Reviews[];
+    transaction_had?: Transaction[];
+    transaction_maked?: Transaction[];
+    coupons_maked?: Coupon[];
     createdAt: string;
     updatedAt: string;
 }
