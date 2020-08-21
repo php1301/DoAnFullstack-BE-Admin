01cebd740f7a0583721c713854433af7f9e71ca2
php1301 <19520854@gm.uit.edu.vn>
2020-08-21 23:54:20 +0700

    StripeService added

--- src/utils/utils.module.ts
+++ src/utils/utils.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { UploadController } from './FilesAndMockUpload';
 import { AwsService } from 'src/aws/aws.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { StripeService } from './stripe';
 
 @Module({
   controllers: [UploadController],
-  providers: [AwsService, PrismaService],
+  providers: [AwsService, PrismaService, StripeService],
+  exports:[StripeService],
 })
 export class UtilsModule {}
