e843ba18ea5d46deca80d9f926ccd32c6a2d14bc
php1301 <19520854@gm.uit.edu.vn>
2020-08-23 23:55:20 +0700

    mailjet added

--- src/services/sendEmail.ts
+++ src/services/sendEmail.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer';
-
+import * as mailjet from 'node-mailjet';
 @Injectable()
 export class MailService {
   constructor(private readonly mailerService: MailerService) {}
@@ -41,6 +41,50 @@ export class MailService {
       })
       .catch(err => {
         console.log(err);
+      });
+  }
+  public mockMailjet(data): void {
+    console.log(data.authorEmail);
+    const request = mailjet
+      .connect(process.env.MJ_APIKEY_PUBLIC, process.env.MJ_APIKEY_PRIVATE)
+      .post('send', { version: 'v3.1' })
+      .request({
+        Messages: [
+          {
+            From: {
+              Email: process.env.LOCAL_MJ_EMAIL,
+              Name: 'Palace TripFinder',
+            },
+            To: [
+              {
+                Email: data.authorEmail,
+                Name: data.authorName,
+              },
+            ],
+            TemplateID: 1645231,
+            TemplateLanguage: true,
+            Subject: `Receipt about ${data.hotelName} Room: ${data.room} Guest: ${data.guest} of ${data.authorName}`,
+            Variables: {
+              firstname: data.authorName,
+              startDate: data.startDate,
+              endDate: data.endDate,
+              hotelName: data.hotelName,
+              guest: data.guest,
+              room: data.room,
+              couponName: data.couponName,
+              total_price: data.total_price,
+              order_date: data.order_date,
+              order_id: data.order_id,
+            },
+          },
+        ],
+      });
+    request
+      .then(result => {
+        console.log(result.body);
+      })
+      .catch(err => {
+        console.log(err.statusCode);
       });
   }
 }
