b6eca0f375a6977a3555699e7f440f35dd2f3551
php1301 <19520854@gm.uit.edu.vn>
2020-08-27 23:15:04 +0700

    gql auth guard context added

--- src/auth/graphql-auth.guard.ts
+++ src/auth/graphql-auth.guard.ts
@@ -2,6 +2,9 @@ import { Injectable, ExecutionContext } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { GqlExecutionContext } from '@nestjs/graphql';
 @Injectable()
+// Nếu muốn sử dụng trực tiếp ko thông qua decorator GqlUser()
+// Hoặc tự config được github strategy thì xài uncomment dòng dưới
+// export class GqlAuthGuard extends AuthGuard(['jwt','facebook-token, google']) {
 export class GqlAuthGuard extends AuthGuard('jwt') {
   getRequest(context: ExecutionContext) {
     const ctx = GqlExecutionContext.create(context);
