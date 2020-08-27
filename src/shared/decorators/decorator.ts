5102769ab8ea8f910ee16a3a3b87aaaf5dde0c62
php1301 <19520854@gm.uit.edu.vn>
2020-08-27 23:18:00 +0700

    decorator context added

--- src/shared/decorators/decorator.ts
+++ src/shared/decorators/decorator.ts
@@ -4,8 +4,9 @@ import { User } from 'src/graphql.schema.generated';
 import { GqlExecutionContext } from '@nestjs/graphql';
 
 // Giống module.export (req, res, next) bên express
-// Để access req và user objects dễ dàng từ grapql ctx thì ta có thể
+// Để access req và user objects dễ dàng từ graphql ctx thì ta có thể
 // Tự tạo custom decorator(thực hiện các logic)
+// Thay thế cho jwt.verify
 // Chi tiết hơn tại: https://docs.nestjs.com/custom-decorators
 export const ResGql = createParamDecorator(
   // (data, [root, args, ctx, info]): Response => ctx.res,
