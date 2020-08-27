0fb9e64778bcfbde56375b6420b7a998f86e555b
php1301 <19520854@gm.uit.edu.vn>
2020-08-27 23:13:38 +0700

    AuthModule modified

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -5,6 +5,7 @@ import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { LoginInput, User, UpdatePassword } from 'src/graphql.schema.generated';
 import { Response } from 'express';
+import { v4 as uuidv4 } from 'uuid';
 import { ResGql, GqlUser } from 'src/shared/decorators/decorator';
 import { SignUpInputDto } from './sign-up-input.dto';
 import { UseGuards } from '@nestjs/common';
@@ -24,13 +25,14 @@ export class AuthResolver {
   ) {
     const user = await this.prisma.client.user({ email });
     if (!user) {
-      throw Error('Email or password incorrect');
+      throw Error('Email or password is incorrect');
     }
     const valid = await bcrypt.compare(password, user.password);
     if (!valid) {
-      throw Error('Email or password incorrect');
+      throw Error('Password is incorrect');
     }
 
+    console.log(process.env.JWT_SECRET);
     const jwt = jwt2.sign(
       {
         id: user.id,
@@ -39,10 +41,109 @@ export class AuthResolver {
       },
       process.env.JWT_SECRET,
     );
-    res.cookie('token', jwt, { httpOnly: true });
+    res.cookie('token', jwt, { httpOnly: false });
     return user;
   }
-
+  @Mutation()
+  async facebookLogin(
+    @Args('email') email,
+    @Args('accessToken') accessToken,
+    @Args('socialInfo') socialInfo,
+    @Args('socialId') socialId,
+    @Args('socialProfileLink') socialProfileLink,
+    @ResGql() res: Response,
+  ) {
+    // Có thể thêm biến type để tạo nhiều dạng social như google, github,...
+    const user = await this.prisma.client.user({ email });
+    const profile_pic_main = `http://graph.facebook.com/${socialId}/picture?width=200&height=200`;
+    if (!user) {
+      const socialUser = await this.prisma.client.createUser({
+        email,
+        username: socialId,
+        first_name: socialInfo,
+        last_name: '',
+        password: uuidv4(),
+        profile_pic_main,
+        cover_pic_main: 'https://i.imgur.com/lXybeGM.png',
+        social_profile: {
+          create: {
+            facebook: socialProfileLink,
+          },
+        },
+        role: 'Normal',
+        // username: signUpInputDto.username,
+      });
+      const jwt = jwt2.sign(
+        {
+          id: socialUser.id,
+          name: socialUser.first_name,
+          accessToken,
+        },
+        process.env.JWT_SECRET,
+      );
+      res.cookie('token', jwt, { httpOnly: false });
+      return socialUser;
+    }
+    // sign JWT hợp lệ
+    // Việc jwt.verify là optional nhờ vào decorator GqlUser()
+    const jwt = jwt2.sign(
+      {
+        id: user.id,
+        name: user.first_name,
+        accessToken,
+      },
+      process.env.JWT_SECRET,
+    );
+    res.cookie('token', jwt, { httpOnly: false });
+    return user;
+  }
+  @Mutation()
+  async googleLogin(
+    @Args('email') email,
+    @Args('accessToken') accessToken,
+    @Args('socialInfo') socialInfo,
+    @Args('socialId') socialId,
+    @Args('profileImage') profileImage,
+    @ResGql() res: Response,
+  ) {
+    // Có thể thêm biến type để tạo nhiều dạng social như google, github,...
+    const user = await this.prisma.client.user({ email });
+    if (!user) {
+      const socialUser = await this.prisma.client.createUser({
+        email,
+        username: socialId,
+        first_name: socialInfo,
+        last_name: '',
+        password: uuidv4(),
+        profile_pic_main: profileImage.replace('s96-c', 's250-c'),
+        cover_pic_main: 'https://i.imgur.com/lXybeGM.png',
+        role: 'Normal',
+        // username: signUpInputDto.username,
+      });
+      const jwt = jwt2.sign(
+        {
+          id: socialUser.id,
+          name: socialUser.first_name,
+          accessToken,
+        },
+        process.env.JWT_SECRET,
+      );
+      res.cookie('token', jwt, { httpOnly: false });
+      return socialUser;
+    }
+    // sign JWT hợp lệ
+    // Việc jwt.verify là optional nhờ vào decorator GqlUser()
+    const jwt = jwt2.sign(
+      {
+        id: user.id,
+        name: user.first_name,
+        accessToken,
+      },
+      process.env.JWT_SECRET,
+    );
+    res.cookie('token', jwt, { httpOnly: false });
+    return user;
+  }
   @Mutation()
   async signup(
     // DTO file eli5: Extends các field của class và gắn thêm các thuộc tính layer
@@ -57,9 +158,12 @@ export class AuthResolver {
       throw Error('Email is already in use');
     }
 
-    const userExists = this.prisma.client.$exists.user({
-      username: signUpInputDto.username,
-    });
+    // Validate bằng username thì uncomment dòng dưới
+
+    // const userExists = this.prisma.client.$exists.user({
+    //   username: signUpInputDto.username,
+    // });
+
     // if (userExists) {
     //   console.log(signUpInputDto.username)
     //   throw Error('Username is already in use');
@@ -73,6 +177,7 @@ export class AuthResolver {
       password,
       first_name: signUpInputDto.first_name,
       last_name: signUpInputDto.last_name,
+      role: 'Normal',
       // username: signUpInputDto.username,
     });
     console.log(process.env.JWT_SECRET);
@@ -84,8 +189,7 @@ export class AuthResolver {
       },
       process.env.JWT_SECRET,
     );
-    console.log(jwt);
-    res.cookie('token', jwt, { httpOnly: true });
+    res.cookie('token', jwt, { httpOnly: false });
     return user;
   }
   @Mutation()
@@ -93,43 +197,78 @@ export class AuthResolver {
   async updatePassword(
     @GqlUser() user: User,
     @Args('password')
-    { oldPassword, confirmPassword, newPassword }: UpdatePassword,
-    @ResGql() res: Response,
+    { oldPassword, confirmPassword, newPassword }: UpdatePassword, // @ResGql() res: Response,
   ) {
     const userValid = await this.prisma.client.user({ id: user.id });
     if (!userValid) {
       throw Error('Must login');
     }
-    console.log('user is logged' + user.id);
 
     const valid = await bcrypt.compare(oldPassword, user.password);
     if (!valid) {
-      throw Error('Old password is not right');
+      throw Error('Old Password is not right');
     }
-    console.log('Same');
+    if (newPassword === oldPassword) {
+      throw Error('New Password and Old Password field must be different');
+    }
     if (confirmPassword !== newPassword) {
       console.log('false');
-      throw Error('Password and new password field must be the same');
+      throw Error('Password Confirm and New Password field must be the same');
     }
     const password = await bcrypt.hash(newPassword, 10);
     console.log('hashed ' + password);
-    const jwt = jwt2.sign(
-      {
-        id: user.id,
-        name: user.first_name + ' ' + user.last_name,
-        roles: 'user',
-      },
-      process.env.JWT_SECRET,
-    );
-    console.log(jwt);
-    res.cookie('token', jwt, { httpOnly: true });
+    // const jwt = jwt2.sign(
+    //   {
+    //     id: user.id,
+    //     name: user.first_name + ' ' + user.last_name,
+    //     roles: 'user',
+    //   },
+    //   process.env.JWT_SECRET,
+    // );
+    // res.cookie('token', jwt, { httpOnly: true });
     return this.prisma.client.updateUser({
       where: {
         id: user.id,
       },
       data: {
-        password
+        password,
       },
     });
+  }
+  @Mutation()
+  async changePasswordFromForgetPassword(
+    @Args('password') password,
+    @Args('email') email,
+    @ResGql() res: Response,
+  ) {
+    try {
+      const userValid = await this.prisma.client.user({ email });
+      if (!userValid) {
+        throw Error('Invalid user');
+      }
+
+      const newPassword = await bcrypt.hash(password, 10);
+      const updatedUser = await this.prisma.client.updateUser({
+        where: {
+          email,
+        },
+        data: {
+          password: newPassword,
+        },
+      });
+      const jwt = jwt2.sign(
+        {
+          id: updatedUser.id,
+          name: updatedUser.first_name,
+          roles: 'user',
+        },
+        process.env.JWT_SECRET,
+      );
+      res.clearCookie('reset-password');
+      // res.cookie('token', jwt, { httpOnly: false });
+      return updatedUser;
+    } catch (e) {
+      console.log(e);
+    }
   }
 }
