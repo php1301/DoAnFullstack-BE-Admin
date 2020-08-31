7e19b93018d2769f0f4491bf4e4f5a7010dbf6f0
php1301 <19520854@gm.uit.edu.vn>
2020-08-31 16:32:43 +0700

    seed modified

--- src/utils/seed.ts
+++ src/utils/seed.ts
@@ -21,7 +21,7 @@ const setup = async (dataMock: any[]) => {
   dataMock.forEach(async i => {
     hotel
       .createHotel({
-        agentId: '5f4bef2f5191510007018ae1',
+        agentId: '5f4cc3945191510007b38333',
         connectId: {
           connect: {
             email: 'duyminhpham1201@gmail.com',
